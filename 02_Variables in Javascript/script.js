157fc99844a8c04a1ed1ef089559ffb338fc91c9
Gousiya <mgousiya@github.com>
2025-02-28 19:57:19 +0530

    javascript

---
+++ 02_Variables in Javascript/script.js
@@ -0,0 +1,6 @@
+let firstName = 'Gousiya'
+let lastName = 'Masool'
+let age = 26
+let isHappy = true
+let userIntro = 'Hi, My name is ' + firstName +' ' + lastName + ' '
+ + 'I am ' + age + ' ' + 'years old'
