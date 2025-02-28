4aa7ef56ac092641e8d7aa67bc149714aa705bb1
Gousiya <mgousiya@github.com>
2025-02-28 23:11:21 +0530

    code running process

---
+++ 03_Code running in dev/script.js
@@ -0,0 +1,10 @@
+debugger
+
+console.log(firstName)
+var firstName = 'Gousiya'
+let lastName = 'Masool'
+let age = 26
+const yearOfBirth = 1998
+
+
+let userIntro = 'Hi, My name is ' + firstName + ' ' + lastName
