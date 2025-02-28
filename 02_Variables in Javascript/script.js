a5d4be1974b0b1e576b59b7e35a1176c09b929e8
Gousiya <mgousiya@github.com>
2025-02-28 20:43:13 +0530

    variables in js

--- 02_Variables in Javascript/script.js
+++ 02_Variables in Javascript/script.js
@@ -1,6 +1,14 @@
-let firstName = 'Gousiya'
+// Variables declared with let can be reassigned, while variables declared with const cannot.
+
+
+console.log(firstName)
+var firstName = 'Gousiya'
+
 let lastName = 'Masool'
 let age = 26
 let isHappy = true
+
+const hoursInDay = 24
+
 let userIntro = 'Hi, My name is ' + firstName +' ' + lastName + ' '
  + 'I am ' + age + ' ' + 'years old'
