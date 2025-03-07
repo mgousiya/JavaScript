e94637e0d9786e31061d6e6af6e8ca9bb64cac96
Gousiya <mgousiya@github.com>
2025-03-07 19:18:43 +0530

    if

---
+++ 10_Decisiom Making in JS using IF statement/script.js
@@ -0,0 +1,16 @@
+const userName = 'Akash'
+const userAge = 28
+
+console.log(`Name:${userName}`);
+console.log(`Age: ${userAge}`);
+
+
+if (userAge >= 18 && userAge <=24)
+{
+    console.log('User is a working Professional');
+    console.log('If condition ran.');
+}
+
+
+console.log('object')
+console.log('Program Ended!!')
