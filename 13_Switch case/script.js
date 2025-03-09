1cc42db295e09ac2a989116ac01adb4c02a4c064
Gousiya <mgousiya@github.com>
2025-03-09 15:07:21 +0530

    switch case

---
+++ 13_Switch case/script.js
@@ -0,0 +1,30 @@
+const dayNumber = 0
+switch (dayNumber){
+    case 0:
+        console.log('It is Sunday today')
+        break
+    case 1:
+        console.log('It is Monday today')
+        break
+    case 2:
+        console.log('It is Tuesday today')
+        break
+    case 3:
+        console.log('It is Wednesday today')
+        break
+    case 4:
+        console.log('It is Thursday today')
+        break
+    case 5:
+        console.log('It is Friday today')
+        break
+    case 6:
+        console.log('It is Saturday today')
+        break
+    default:
+        console.log('Please enter a valid number')
+
+        
+}
+
+console.log('Program Ended!')
