dfbc162c6049318544efc2f0f02b5fb298e1dc15
Gousiya <mgousiya@github.com>
2025-03-23 20:04:51 +0530

    Array methods

---
+++ 19_Basic Array methods/script.js
@@ -0,0 +1,10 @@
+const evenNumbers = [0,2,4,6,8]
+const animals = ["cat","dog","rat"]
+const oddNumber=[1,3,5]
+
+
+// evenNumbers.shift()
+// evenNumbers.unshift(0)
+
+const addedArray1 = evenNumbers.concat(animals)
+const addedArray2 = animals.concat(evenNumbers,oddNumber)
