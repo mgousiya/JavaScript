ac63a696ff79397f74a3c0591fbb0d4ab26e053d
Gousiya <mgousiya@github.com>
2025-04-05 18:15:44 +0530

    M arrays

---
+++ 20_Multidimensional arrays/script.js
@@ -0,0 +1,13 @@
+const nameAndNumberList = [
+    ['Adarsh', 75], 
+    ['Akash', 90],
+    ['Anurag', 9]
+]
+
+
+const ticTacToe = [
+    ['X',null,null],
+    [null,null,'O'],
+    ['O',null,'X']
+]
+
