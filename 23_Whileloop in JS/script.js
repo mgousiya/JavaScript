2fba239f3706523402746d8aeae1e8b37db689f1
Gousiya <mgousiya@github.com>
2025-04-05 22:34:04 +0530

    whileloop in JS

---
+++ 23_Whileloop in JS/script.js
@@ -0,0 +1,25 @@
+console.log('Program Started');
+
+// let num = 0;
+
+// while(num < 10){
+//     console.log(num);
+//     num = num + 1
+// }
+
+
+// let num = 1;
+// while(num <= 100){
+//     console.log(num);
+//     num = num + 1
+// }
+
+const friends = ['Tabrez','Tahir','Arshad','Mansoor','Ayaan']
+
+let i = 0;
+while(i < 5){
+    console.log(friends[i]);
+    i = i +1
+
+}
+console.log('Program Ended');
