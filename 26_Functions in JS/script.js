c922fa227577ab2e7062bea5dd02e470e15e689e
Gousiya <mgousiya@github.com>
2025-04-06 20:20:59 +0530

    functions in js

--- 26_Functions in JS/script.js
+++ 26_Functions in JS/script.js
@@ -4,8 +4,26 @@
 
 // console.log('****************************');
 
-function introduceMe() {
+// function introduceMe() {
+//     console.log('Hi,');
+//     console.log('My name is Gousiya.');
+//     console.log('I am a web developer');
+// }
+
+
+// Function Definition
+function introduceMe(username, profession, age) {
+    // console.log(typeof username);
+    // console.log(typeof profession);
+    // console.log(typeof age);
     console.log('Hi,');
-    console.log('My name is Gousiya.');
-    console.log('I am a web developer');
+    console.log(`My name is ${username || 'masool'}.`);
+    console.log(`I am a ${profession}`);
+    console.log(`I am ${age} years old.`); 
 }
+
+// //                  Function Call
+// const returnValue = introduceMe()
+
+// introduceMe('Gousiya', 'Software Engineer', 25)
+// introduceMe('Eliyaz', 'Mechanical Engineer', 27)
