ee75502df707ca645c57bb78e75d26b9e09e3026
Gousiya <mgousiya@github.com>
2025-04-06 18:30:59 +0530

    Functions in JS

---
+++ 26_Functions in JS/script.js
@@ -0,0 +1,11 @@
+// console.log('Hi,');
+// console.log('My name is Gousiya.');
+// console.log('I am a web developer');
+
+// console.log('****************************');
+
+function introduceMe() {
+    console.log('Hi,');
+    console.log('My name is Gousiya.');
+    console.log('I am a web developer');
+}
