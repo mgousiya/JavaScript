ac07541ffc273e3d8aa516e2c9462b22611e9840
Gousiya <mgousiya@github.com>
2025-04-07 21:03:59 +0530

    call stack

---
+++ 29_Call stack in JS/script.js
@@ -0,0 +1,30 @@
+debugger
+
+// sayHi()
+
+// const username = 'Gousiya'
+// const userAge = 25
+
+// function sayHi() {
+//     const a = 14
+//     const b = 12
+//     add(7, 9)
+// }
+
+// function add(x, y) {
+//     kuchhBhi()
+//     return x + y
+// }
+
+// function kuchhBhi() {
+//     console.log('Kuchh bhi');
+// }
+
+function introduceMe() {
+    console.log('Hi, My name is Gousiya.');
+    introduceMe()
+}
+
+introduceMe()
+
+console.log('Program Ended');
