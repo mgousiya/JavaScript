509d2d6a2d7e8a003f2d5ae36554ad303863c997
Gousiya <mgousiya@github.com>
2025-04-07 19:28:34 +0530

    Return keyword in JS

---
+++ 27_Return keyword in js/script.js
@@ -0,0 +1,15 @@
+// function addTwoNumbers() {
+//     console.log(5 + 6);
+// }
+
+// addTwoNumbers()
+
+function addTwoNumbers(a, b) {
+    debugger
+    
+        return a + b
+    }
+    
+    // const result = addTwoNumbers(56, 6)
+    
+    // console.log(result);
